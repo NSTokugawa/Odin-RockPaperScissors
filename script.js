7459a260909d3de24271a14cb529dbb80f49ac69
NSTokugawa <thomas@crassflag.com>
2022-01-03 05:03:43 -0800

    Basic functionality completed

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let computerChoice = getRandomInt(3);
+let computerSelection = getRandomInt(3);
+let playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors?");
+let computerPlay = computerRandom(computerSelection);
+let result = battle(playerSelection,computerSelection);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 };
 
-function computerPlay() {
-  if (computerChoice === 0) {
+function computerRandom() {
+  if (computerSelection === 0) {
   return "rock";
-} else if (computerChoice === 1) {
+} else if (computerSelection === 1) {
   return "paper";
-} else if (computerChoice === 2) {
+} else if (computerSelection === 2) {
   return "scissors";
 }
 };
+
+function battle() {
+  if (playerSelection === "rock" && computerPlay === "scissors" || playerSelection === "scissors" && computerPlay === "paper" || playerSelection === "paper" && computerPlay === "rock") {
+    return "Player Wins!";
+  } else if (playerSelection === computerPlay) {
+    return "Game is a Draw!";
+  } else {
+    return "Computer Wins!";
+  }
+};
+
+alert(result);
