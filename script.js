e35a4359dc87e327275a870da9dfade8196b6bf7
Thomas C. Abernathy <thomas@crassflag.com>
2022-01-03 19:20:10 -0800

    Actually working

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Prompts user for input
 let playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors?");
 // Calls function getRandomInt to fetch a random number for function computerRandom
-let computerSelection = getRandomInt(3);
+//let computerSelection = getRandomInt(3);
 // Calls function computerRandom to fetch computer's input for function battle
-let computerPlay = computerRandom(computerSelection);
-// Assigns result of function battle to a variable for printing
-let result = battle(playerSelection,computerSelection);
+let computerPlay = computerRandom(getRandomInt(3));
 
 // Generates a random number 0, 1, 2
 function getRandomInt(max) {
@@ -14,20 +12,20 @@ function getRandomInt(max) {
 
 // Converts random number into playable variable
 function computerRandom() {
-  if (computerSelection === 0) {
+  if (getRandomInt(3) === 0) {
   return "rock";
-} else if (computerSelection === 1) {
+} else if (getRandomInt(3) === 1) {
   return "paper";
-} else if (computerSelection === 2) {
+} else if (getRandomInt(3) === 2) {
   return "scissors";
 }
 };
 
 // Checks player input against computer input to determine game outcome
 function battle() {
-  //playerSelection = playerSelection.toLowerCase();
-  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSeceltion !== "scissors") {
-    alert("Please choose");
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+    alert("Please choose 'Rock', 'Paper', or 'Scissors'");
   } else if (playerSelection === "rock" && computerPlay === "scissors" || playerSelection === "scissors" && computerPlay === "paper" || playerSelection === "paper" && computerPlay === "rock") {
     return "Player Wins!";
   } else if (playerSelection === computerPlay) {
@@ -38,4 +36,4 @@ function battle() {
 };
 
 // Logs game outcome
-console.log(result);
+console.log(battle(playerSelection,computerPlay));
